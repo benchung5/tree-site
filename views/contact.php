5b689ec9f106bef215b9497bfcb1ae1e1030d76e
Ben Chung <ben@benchung.com>
2025-05-03 00:54:11 -0700

    updated

--- views/contact.php
+++ views/contact.php
@@ -9,7 +9,7 @@
 						<div class="title-area">
 							<h1>Contact Nature With Us</h1>	
 						</div>
-						<p>The below map is a rough idea of our service area, covering Prince George, Pineview, Buckhorn, Redrock, Stoner, Woodpecker, down to Hixon. It's just an approximation so feel free to give us a call either way!</p>
+						<p>The below map is a rough idea of our service area, covering Prince George, Pineview, Buckhorn, down to Redrock. It's just an approximation so feel free to give us a call either way!</p>
 						<iframe src="https://www.google.com/maps/d/embed?mid=1R4HEbUlvqpoFmto7KERDn2qTv1g3JMo&ehbc=2E312F" width="100%" height="480"></iframe>
 					</div>
 					<div class="small-12 large-5 columns sidebar">
